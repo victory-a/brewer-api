1c94380257dfdc234b1bc48dbe5608666077f8ae
victory-a <victoryasokomeh@gmail.com>
2023-10-28 23:47:22 +0100

    wip: scaffold API

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const app = express();
+
+app.use(express.json());
+const prisma = new PrismaClient({
+  // log: ['query', 'info', 'warn', 'error'],
+});
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  // const allUsers = await prisma.user.findMany();
+  // console.log(allUsers);
+  // await prisma.user.create({
+  //   data: {
+  //     name: 'Alice',
+  //     email: 'alice@prisma.io',
+  //     posts: {
+  //       create: { title: 'Hello World' },
+  //     },
+  //     profile: {
+  //       create: { bio: 'I like turtles' },
+  //     },
+  //   },
+  // })
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  });
+  console.dir(allUsers, { depth: null });
+}
+
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect();
+//   })
+//   .catch(async (e) => {
+//     console.error(e);
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   });
+
+const server = app.listen(3000, () => console.log(`🚀 Server ready at: http://localhost:3000`));
