135e3dfa9c63dd8fe41ac2619a1cbebfcef53a8f
victory-a <victoryasokomeh@gmail.com>
2023-10-29 00:30:42 +0100

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import express from 'express';
-import { env } from 'process';
+const { PrismaClient } = require('@prisma/client');
 
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 
-const port = env.PORT;
+const prisma = new PrismaClient();
+
+// define routes here
+
 const server = app.listen(port, () => console.log(`🚀 Server ready at: http://localhost:${port}`));
+
+// Handle graceful shutdown
+process.on('beforeExit', async () => {
+  // Disconnect Prisma Client before the application exits
+  await prisma.$disconnect();
+});
 
 // Handle global unhandled promise rejections
 process.on('unhandledRejection', (err: Error, data) => {
