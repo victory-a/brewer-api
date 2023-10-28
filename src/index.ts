18938c8acf25653b079cdee4e6e785de68954404
victory-a <victoryasokomeh@gmail.com>
2023-10-29 00:09:31 +0100

    scaffold directories

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,9 @@
 import express from 'express';
-import { PrismaClient } from '@prisma/client';
 import { env } from 'process';
 
 const app = express();
 
 app.use(express.json());
-const prisma = new PrismaClient({
-  // log: ['query', 'info', 'warn', 'error'],
-});
-
-async function main() {
-  // ... you will write your Prisma Client queries here
-  // const allUsers = await prisma.user.findMany();
-  // console.log(allUsers);
-  // await prisma.user.create({
-  //   data: {
-  //     name: 'Alice',
-  //     email: 'alice@prisma.io',
-  //     posts: {
-  //       create: { title: 'Hello World' },
-  //     },
-  //     profile: {
-  //       create: { bio: 'I like turtles' },
-  //     },
-  //   },
-  // })
-
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-      profile: true,
-    },
-  });
-  console.dir(allUsers, { depth: null });
-}
-
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect();
-//   })
-//   .catch(async (e) => {
-//     console.error(e);
-//     await prisma.$disconnect();
-//     process.exit(1);
-//   });
 
 const port = env.PORT;
 const server = app.listen(port, () => console.log(`🚀 Server ready at: http://localhost:${port}`));
