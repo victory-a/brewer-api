04758fcfb549638a02c07b91deba35fab482ada5
victory-a <victoryasokomeh@gmail.com>
2023-10-30 01:23:34 +0100

    config

---
+++ src/config/index.ts
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({ path: path.join(__dirname, '../../.env') });
+
+module.exports = {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  DATABASE_URL: process.env.DATABASE_URL,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 const { PrismaClient } = require('@prisma/client');
 
+const config = require('./config/index');
+
 const app = express();
-const port = process.env.PORT || 4000;
+const port = config.port || 4000;
 
 app.use(express.json());
 
@@ -14,12 +16,25 @@ const server = app.listen(port, () => console.log(`🚀 Server ready at: http://
 
 // Handle graceful shutdown
 process.on('beforeExit', async () => {
-  // Disconnect Prisma Client before the application exits
   await prisma.$disconnect();
 });
 
 // Handle global unhandled promise rejections
-process.on('unhandledRejection', (err: Error, data) => {
+const unexpectedErrorHandler = (err: Error) => {
   console.log(`Error ❌: ${err.message}`);
-  server.close(() => process.exit(1));
+  if (server) {
+    server.close(() => process.exit(1));
+  } else {
+    process.exit(1);
+  }
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  console.log(`Error ❌: Received signal to terminate`);
+  if (server) {
+    server.close();
+  }
 });
