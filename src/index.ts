f62045f1466c50b8f07ad64748e97d587ab3a5af
victory-a <victoryasokomeh@gmail.com>
2023-11-06 00:23:34 +0100

    ci: setup linter

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,39 @@
 import express from 'express';
-const { PrismaClient } = require('@prisma/client');
 
 import authRoutes from './routes/authRoutes';
+
+const { PrismaClient } = require('@prisma/client');
 
 const config = require('./config/index');
 
 const app = express();
-const port = config.port || 4000;
+const port = config.port ?? 4000;
 
 app.use(express.json());
 app.use('/auth', authRoutes);
 
 const prisma = new PrismaClient();
 
-// define routes here
-
-const server = app.listen(port, () => console.log(`🚀 Server ready at: http://localhost:${port}`));
+const server = app.listen(port, () => {
+  console.log(`🚀 Server ready at: http://localhost:${port}`);
+});
 
 // Handle graceful shutdown
-process.on('beforeExit', async () => {
-  await prisma.$disconnect();
+process.on('beforeExit', () => {
+  prisma
+    .$disconnect()
+    .then(() => {
+      console.log('Prisma disconnected successfully.');
+    })
+    .catch((error: Error) => {
+      console.error(`Error disconnecting from Prisma: ${error.message}`);
+    });
 });
 
 // Handle global unhandled promise rejections
-const unexpectedErrorHandler = (err: Error) => {
+const unexpectedErrorHandler = (err: Error): void => {
   console.log(`Error ❌: ${err.message}`);
-  if (server) {
+  if (server !== undefined) {
     server.close(() => process.exit(1));
   } else {
     process.exit(1);
@@ -36,8 +44,8 @@ process.on('uncaughtException', unexpectedErrorHandler);
 process.on('unhandledRejection', unexpectedErrorHandler);
 
 process.on('SIGTERM', () => {
-  console.log(`Error ❌: Received signal to terminate`);
-  if (server) {
+  console.log('Error ❌: Received signal to terminate');
+  if (server !== undefined) {
     server.close();
   }
 });
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,8 +2,8 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.post("login", async (req, res) => {
-
-})
+router.post('login', async (req, res) => {
+  return {};
+});
 
 export default router;
