c4dc5e07575ba734910a904793f948577ebbb8b0
victory-a <victoryasokomeh@gmail.com>
2023-11-16 09:35:14 +0100

    feat: get current user

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 const router = Router();
 
-const { login, authenticate } = require('../controller/auth.controller');
+const { login, authenticate, currentUser } = require('../controller/auth.controller');
+const { protect } = require('../middlewares/auth.middleware');
 
 router.post('/login', login);
-
 router.post('/authenticate', authenticate);
+
+router.get('/current-user', protect, currentUser);
 
 module.exports = router;
