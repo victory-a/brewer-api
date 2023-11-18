4e2bb1f22e14f2112d68f458d8dbf4bea5281405
victory-a <victoryasokomeh@gmail.com>
2023-11-18 20:12:26 +0100

    cleanup error handler

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,26 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-const { ErrorResponse } = require('../utils/apiResponder');
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-const errorHandler = (err, req, res, next) => {
-  let error = { ...err };
+const errorHandler = (err, _, res) => {
+  const error = { ...err };
 
   error.message = err.message;
 
-  if (err.name === 'CastError') {
-    const message = 'Resource not found';
-    error = new ErrorResponse(message, 404);
-  }
-
-  if (err.code === 11000) {
-    const message = 'Duplicate field value entered';
-    error = new ErrorResponse(message, 400);
-  }
-
   res.status(error.statusCode || 500).json({
     success: false,
-    error: error.message || 'Server Error'
+    message: error.message || 'Server Error'
   });
 };
 
