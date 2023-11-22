860eda1dcc7fc5c2bc043ebcab4f76e892809f53
victory-a <victoryasokomeh@gmail.com>
2023-11-22 01:09:17 +0100

    add logger

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,17 @@
 import express from 'express';
-import morgan from 'morgan';
 const { PrismaClient } = require('@prisma/client');
 
 const authRoutes = require('./routes/auth.route');
 
 const purgeDatabase = require('./scripts/purgeDatabase');
+const logger = require('./utils/logger');
 
 const errorHandler = require('./middlewares/error.middleware');
 const config = require('./config/index');
 
 const app = express();
 
-// DEV LOGGER
-morgan.token('req-headers', function (req, res) {
-  return JSON.stringify(req.headers);
-});
-process.env.NODE_ENV === 'development' && app.use(morgan(':method :url :status :req-headers'));
+logger(app);
 
 purgeDatabase();
 
---
+++ src/utils/logger.ts
@@ -0,0 +1,12 @@
+import { type Express } from 'express';
+
+import morgan from 'morgan';
+
+function logger(app: Express) {
+  morgan.token('req-headers', function (req, res) {
+    return JSON.stringify(req.headers);
+  });
+  process.env.NODE_ENV === 'development' && app.use(morgan(':method :url :status :req-headers'));
+}
+
+module.exports = logger;
