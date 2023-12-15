2447cf3abe0c4eeca55390dbf2dca2bea108f1f4
victory-a <victoryasokomeh@gmail.com>
2023-12-15 22:50:09 +0100

    create order endpoint

--- src/controller/order.controller.ts
+++ src/controller/order.controller.ts
@@ -1,9 +1,25 @@
 import { type Request, type Response } from 'express';
-import { PrismaClient, type Product } from '@prisma/client';
+import { type Order } from '@prisma/client';
 
 import { successResponse, errorResponse } from '../utils/apiResponder';
 import asyncHandler from '../utils/asyncHandler';
 
 const prisma = require('../models/db');
 
-// const createOrder()
+const createOrder = asyncHandler(async (req: Request, res: Response) => {
+  const order = req.body as Order;
+
+  try {
+    const createdOrder = await prisma.order.create({
+      data: order
+    });
+
+    successResponse(res, createdOrder, 'Order Created Successfully');
+  } catch (error) {
+    errorResponse(res, 'Failed to create Order', 400);
+  }
+});
+
+module.exports = {
+  createOrder
+};
