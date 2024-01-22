a61cff13ac4317ebc18304a1ecdfbc836c7d893a
victory-a <victoryasokomeh@gmail.com>
2024-01-22 07:30:59 +0100

    feat: update create-product validation

--- src/validations/product.validations.ts
+++ src/validations/product.validations.ts
@@ -6,7 +6,6 @@ const createProductValidation = {
     description: Joi.string().max(255).required(),
     variant: Joi.string().max(20).required(),
     image: Joi.string().uri().required(),
-    // size: Joi.string().valid('small', 'medium', 'large').required(),
     sizes: Joi.array().items(Joi.string().valid('small', 'medium', 'large')),
     rating: Joi.number().precision(1).min(1).max(5).required(),
     basePrice: Joi.number().min(0).required(),
