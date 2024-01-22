38535892a506c9aa9505beb542bc9afb38bdd1ff
victory-a <victoryasokomeh@gmail.com>
2024-01-22 07:31:34 +0100

    feat: arbitary comit to test squash

--- src/validations/product.validations.ts
+++ src/validations/product.validations.ts
@@ -7,11 +7,11 @@ const createProductValidation = {
     variant: Joi.string().max(20).required(),
     image: Joi.string().uri().required(),
     sizes: Joi.array().items(Joi.string().valid('small', 'medium', 'large')),
-    rating: Joi.number().precision(1).min(1).max(5).required(),
     basePrice: Joi.number().min(0).required(),
     small: Joi.number().min(0),
     medium: Joi.number().min(0),
-    large: Joi.number().min(0)
+    large: Joi.number().min(0),
+    rating: Joi.number().precision(1).min(1).max(5).required()
   })
 };
 
